a055ad3fbac58421e51c1788f7318a09dc18db25
Zeerex23 <tobilobadiekola@gmail.com>
2022-06-21 01:50:48 +0100

    Feat: Creating App Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
