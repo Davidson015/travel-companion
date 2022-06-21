4bd456556f70d3c113d843d61eb1acb991f89a8b
Zeerex23 <tobilobadiekola@gmail.com>
2022-06-21 01:12:40 +0100

    Refactor: Re-arranging Folder Structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
